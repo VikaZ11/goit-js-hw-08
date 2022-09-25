7a2d19810d1f56ab455cae63a11407356da99fc7
VikaZ11 <104565629+VikaZ11@users.noreply.github.com>
2022-09-25 23:25:18 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+let saveData = localStorage.getItem('videoplayer-current-time');
+if (saveData) {
+    player.setCurrentTime(saveData);
+}
+
 
