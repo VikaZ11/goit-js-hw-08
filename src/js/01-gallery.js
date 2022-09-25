b96b979a130aed270d205e663379ae183ea30a05
VikaZ11 <104565629+VikaZ11@users.noreply.github.com>
2022-09-25 21:56:17 +0200

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const images = galleryItems
+  .map(img => {
+    return `<div class="gallery__item">
+      <a class="gallery__item" href="${img.original}">
+         <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+        </a>
+    </div>`;
+  })
+  .join('');
+
+galleryRef.innerHTML = images;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
+
