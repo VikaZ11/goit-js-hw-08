594d3911b63e74b3344d9e7d3fc534f4eeef246f
VikaZ11 <104565629+VikaZ11@users.noreply.github.com>
2022-09-26 08:48:42 +0200

    add storage.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,17 @@
 import Player from '@vimeo/player';
+import storage from './storage';
 var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 const onPlay = function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
+    storage.save('videoplayer-current-time', data.seconds);
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-let saveData = localStorage.getItem('videoplayer-current-time');
+let saveData = storage.load('videoplayer-current-time');
 if (saveData) {
     player.setCurrentTime(saveData);
 }
